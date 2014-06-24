3f27f288145169c352bc637ab00bf01c6ad99fa7
Ross Guarino <rssguar@gmail.com>
2014-06-23 21:01:42 -0400

    Initial implementation using Hibernate ORM and Authentication.
    The first working code using hibernate to persist the Userd object
    to the database. The User entity is called Userd because User is a
    keyword in Postgresql and to avoid unnecesary configuration it is easier to just
    use Userd for User(database) at the Model.
    
    For reference this is how to use the jpa: http://www.objectdb.com/java/jpa/query
    
    Also, the first working code that implements authentication. Using the
    @Security.Authenticated(Secured.class) we can make sure that some
    resources are only displayed to logged in users.
    
    I followed this guide: http://www.playframework.com/documentation/2.1.0/JavaGuide4 along with this https://github.com/yesnault/PlayStartApp to implement the authentication.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,13 +2,108 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
+import play.db.jpa.JPA;
+import play.db.jpa.Transactional;
+import play.data.*;
+import play.data.Form;
+import play.data.validation.Constraints;
 
+import static play.data.Form.form;
+
+
+// App imports
 import views.html.*;
+import models.Userd;
 
 public class Application extends Controller {
 
+	@Transactional
+	@Security.Authenticated(Secured.class)
     public static Result index() {
         return ok(index.render("Your new application is ready."));
     }
+  
 
+    public static class Register {
+    	/**
+		* Register Class taken from https://github.com/yesnault/PlayStartApp/
+	 	*/
+    	@Constraints.Required
+    	public String email;
+
+    	@Constraints.Required
+    	public String fullname;
+
+    	@Constraints.Required
+    	public String inputPassword;
+
+    	/**
+         * Validate the authentication.
+         *
+         * @return null if validation ok, string with details otherwise
+         */
+    	public String validate(){
+    		if (isBlank(email)){
+    			return "Email is required";
+    		}
+    		if (isBlank(fullname)){
+    			return "Full name is required";
+    		}
+    		if (isBlank(inputPassword)){
+    			return "Password is required";
+    		}
+
+    		return null;
+    	}
+
+    	private boolean isBlank(String input) {
+            return input == null || input.isEmpty() || input.trim().isEmpty();
+        }
+    }
+
+    public static class Login {
+
+    	@Constraints.Required
+    	public String email;
+    	@Constraints.Required
+    	public String password;
+
+
+    	public String validate(){
+    		if (isBlank(email)){
+    			return "Email is required";
+    		}
+    		if (isBlank(password)){
+    			return "Password is required";
+    		}
+    		//TODO: Catch exceptions here.
+    		if( Userd.authenticate(email, password) == null){
+    			return "Invalid user or password";
+    		}
+    		return null;
+
+
+    	}
+
+    	private boolean isBlank(String input) {
+            return input == null || input.isEmpty() || input.trim().isEmpty();
+        }
+
+	}
+
+    public static Result login(){
+    	return ok(login.render(form(Login.class)));
+    }
+
+    @Transactional
+    public static Result authenticate(){
+    	Form<Login> loginForm = form(Login.class).bindFromRequest();
+    	if (loginForm.hasErrors()){
+    		return badRequest(login.render(loginForm));
+    	} else {
+    		session().clear();
+    		session("email", loginForm.get().email);
+    		return redirect(routes.Application.index());
+    	}
+    }
 }
--- build.sbt
+++ build.sbt
@@ -8,10 +8,10 @@ scalaVersion := "2.11.1"
 
 libraryDependencies ++= Seq(
   javaJdbc,
-  javaEbean,
+  javaJpa,
   cache,
-  javaWs
+  javaWs,
+  "org.hibernate" % "hibernate-entitymanager" % "3.6.9.Final",
+  "org.postgresql" % "postgresql" % "9.3-1101-jdbc41"
 )
 
-
-libraryDependencies += "org.postgresql" % "postgresql" % "9.3-1101-jdbc41"
