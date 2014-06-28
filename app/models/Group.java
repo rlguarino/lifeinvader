fa4f60dfe7e06a7d391183a8d1bef511aa459f12
rxl7906 <rxl7906@rit.edu>
2014-06-28 18:00:08 -0400

    Group commit

--- app/models/Group.java
+++ app/models/Group.java
@@ -1,23 +1,50 @@
 package models;
 
 import play.db.jpa.JPA;
-
+import play.*;
+import play.mvc.*;
+import play.db.*;
+import java.sql.*;
 import play.Logger;
+import java.util.Calendar;
+import java.sql.Timestamp;
+import java.sql.ResultSet;
+import java.util.Date;
 
 
 //Group size initialized 
 //
 public class Group {
 
-	public Long id;
-
+	public Long groupid;
 	public String name;
 	public int groupsize;
 
-	public static String ID = "user_id";
-	public static String name = "name";
+	public static String GROUP = "group"; // Table Name is Group
+	public static Long GROUP_ID = "groupid";
+	public static String NAME = "name";
 	public static int = 0;
 	
+	/**
+     * dquote
+     * double quotes the given string in \", for use in sql querries.
+     * @param str the string to quote
+     * @return the quoted string
+     */
+    public static String dquote(String str){
+        return "\"" + str + "\"";
+    }
+
+    /**
+     * quote
+     * single quotes the given string in \", for use in sql querries.
+     * @param str the string to quote
+     * @return the quoted string
+     */
+    public static String quote(String str){
+        return "\'" + str + "\'";
+    }
+
 	/**
 	*
 	* Constructor
@@ -27,8 +54,8 @@ public class Group {
 	* @param groupsize: The groupsize 
 	*
 	*/
-	public Group (Long id, String name, int groupsize) {
-	this.id = id;
+	public Group (Long groupid, String name, int groupsize) {
+	this.groupid = groupid;
 	this.name = name;
 	this.groupsize = groupsize;
 	} 
@@ -39,6 +66,15 @@ public class Group {
 	* create a new database connection
 	*/
 	public void Persist(){
+
+		PreparedStatement = pstmt = null;
+		Connection conn = null;
+		try {
+			conn = DB.getConnection();
+			String sql = "UPDATE" + Group.dquote(Group.GROUP);
+				sql += " set " + Group.NAME + " = ?, ";
+				sql += Group.groupsize
+		}
 
 		//TODO
 		return null;
