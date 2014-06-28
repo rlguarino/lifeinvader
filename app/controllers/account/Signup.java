fe4f5bf1d7826db6cb8d3620767d90166339edce
Ross Guarino <rssguar@gmail.com>
2014-06-28 10:35:18 -0400

    Moved the registration form to the Signup controller

--- app/controllers/account/Signup.java
+++ app/controllers/account/Signup.java
@@ -16,6 +16,47 @@ import models.Userd;
 public class Signup extends Controller {
 
 	/**
+	 * Registration form
+	 */
+    public static class Register {
+
+        @Constraints.Required
+        public String email;
+
+        @Constraints.Required
+        public String fullname;
+
+        @Constraints.Required
+        public String inputPassword;
+
+        /**
+         * Validate the registration form.
+         *
+         * @return null if validation ok, string with details otherwise
+         */
+        public String validate(){
+            if (isBlank(email)){
+                return "Email is required";
+            }
+            if (isBlank(fullname)){
+                return "Full name is required";
+            }
+            if (isBlank(inputPassword)){
+                return "Password is required";
+            }
+
+            // TODO Actually add the new user to the database
+            // Remember to check for duplicate email
+
+            return null;
+        }
+
+        private boolean isBlank(String input) {
+            return input == null || input.isEmpty() || input.trim().isEmpty();
+        }
+    }
+
+	/**
      * Display registration page.
      */
     public static Result registration(){
