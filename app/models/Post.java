11825749acc1aab350455f2a82d10f139d5ad448
Zach Huenink <zhuenink@gmail.com>
2014-07-07 16:32:12 -0400

    Added Find by Id, and Delete functionality to posts

--- app/models/Post.java
+++ app/models/Post.java
@@ -18,14 +18,16 @@ public class Post{
 	public Timestamp time_stamp;
 	public Long user_ID;
 	public Long post_ID;
+	public Long wall_ID;
 
 	// Table column names
 	public static String POST = "post"; // Table Name is post
-	public static String CONTENT = "Content"; 
+	public static String CONTENT = "content"; 
 	public static String TYPE = "type";
-	public static String TIMESTAMP = "time_stamp";
-	public static String USER_ID = "user_ID";
-	public static String POST_ID = "post_ID";
+	public static String TIMESTAMP = "timestamp";
+	public static String USER_ID = "userid";
+	public static String POST_ID = "postid";
+	public static String WALL_ID = "wallid";
 	
 	/**
 	*
@@ -38,17 +40,22 @@ public class Post{
 	* @param postID: 		The id of the post
 	*
 	*/
-	public Post (String content, String type, Timestamp time_stamp, Long user_ID, Long post_ID) {
+	public Post (String content, String type, Timestamp time_stamp, Long user_ID, Long post_ID, Long wall_ID) {
 		this.content = content;
 		this.type = type;
 		this.time_stamp = time_stamp;
 		this.user_ID = user_ID;
 		this.post_ID = post_ID;
-	} 
+		this.wall_ID = wall_ID;
+	}
+
+	public static String dquote(String str){
+        return "\"" + str + "\"";
+    }
 
 	/**
 	* Persist
-	* Saves the wall to the database, this function will
+	* Saves the post to the database, this function will
 	* create a new database connection
 	*/
 	public void Persist(){
@@ -62,6 +69,7 @@ public class Post{
 				sql += Post.TYPE + " = ?, ";
 				sql += Post.TIMESTAMP + " = ?, ";
 				sql += Post.USER_ID + " = ?, ";
+				sql += Post.WALL_ID + " = ?, ";
 				sql += "where " + Post.POST_ID + " = ?";
 
 			Logger.debug("Generated update: [%s]", sql);
@@ -70,7 +78,8 @@ public class Post{
 			pstmt.setString(2, this.type);
 			pstmt.setTimestamp(3, this.time_stamp);
 			pstmt.setLong(4, this.user_ID);
-			pstmt.setLong(5, this.post_ID);
+			pstmt.setLong(5, this.wall_ID);
+			pstmt.setLong(6, this.post_ID);
 
 			pstmt.executeUpdate();
 
@@ -78,7 +87,7 @@ public class Post{
 			conn.close();
         } catch(SQLException e){
             // Attempt to close the connection
-            Logger.debug("Error while persiting wall");
+            Logger.debug("Error while persisting Post");
             if (conn != null){
                 try{
                 conn.close();
@@ -96,8 +105,68 @@ public class Post{
      * @param connection    The jdbc connection
      * @param id            The id to select by
      */
-     public static Group FindByID(){
-     	//TODO
-     	return null;
-     }
+     public static Post FindByID(Long post_ID) throws SQLException{
+     	Post post = null;
+     	PreparedStatement findPostId = null;
+     	Connection conn = null;
+
+     	String sql = String.format("SELECT * from %s where postid = ?",
+     		Post.dquote(POST), post_ID);
+
+
+     	if(post_ID != null){
+     		try{
+     			conn = DB.getConnection();
+     			findPostId = conn.prepareStatement(sql);
+     			findPostId.setLong(1, post_ID);
+
+     			ResultSet rs = findPostId.executeQuery();
+
+     			if(rs.next()){
+     				post = new Post(
+     					rs.getString(Post.CONTENT),
+     					rs.getString(Post.TYPE),
+     					rs.getTimestamp(Post.TIMESTAMP),
+     					rs.getLong(Post.USER_ID),
+     					rs.getLong(Post.WALL_ID),
+     					rs.getLong(Post.POST_ID));
+     			}
+     		
+     		findPostId.close();
+     		conn.close();
+     		return post;
+     		}catch (SQLException e){
+     			Logger.debug("Error retrieving post.", e);
+     		}
+     	}
+     	return post;
+    }
+
+    public static void deletePost(Long post_ID) throws SQLException{
+    	Post post = null;
+
+    	String deletePostStr = String.format("DELETE from %s where postid = ?",
+    		Post.dquote(POST), post_ID);
+
+    	Connection conn = null;
+    	PreparedStatement deleteP = null;
+
+    	if(post_ID != null){
+    		try{
+    			conn = DB.getConnection();
+    			deleteP = conn.prepareStatement(deletePostStr);
+    			ResultSet rs = deleteP.executeQuery();
+
+    			if(rs.next()){
+    				Logger.debug("Failed to delete the Post.");
+    			}
+    		deleteP.close();
+    		conn.close();
+    		}catch (SQLException e){
+    			Logger.debug("Failed while trying to delete the post.");
+    		}
+    	}else{
+    		Logger.debug("Post id is null.");
+    	}
+    }
 }
